80346e2af4085353c374c36243b58dedb9d59657
RadimP <RadimP@192.168.0.213>
2018-03-05 10:39:51 +0100

    zkopirovan a vyzkousen kód pro zasílání souboru

---
+++ zaslani_souboru/src/zaslani_souboru/SimpleServer.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zaslani_souboru;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+/**
+ *
+ * @author RadimP
+ */
+public class SimpleServer {
+public final static int SOCKET_PORT = 13267;  // you may change this
+  public final static String FILE_TO_SEND = "c:\\Users\\RadimP\\Documents\\NetBeansProjects\\zaslani_souboru_po_siti\\zaslani_souboru_po_siti\\zaslani_souboru\\AGRI-2015-80319-00-00-EN-ORI-00.DOCX";  // you may change this
+
+  public static void main (String [] args ) throws IOException {
+    FileInputStream fis = null;
+    BufferedInputStream bis = null;
+    OutputStream os = null;
+    ServerSocket servsock = null;
+    Socket sock = null;
+    try {
+      servsock = new ServerSocket(SOCKET_PORT);
+      while (true) {
+        System.out.println("Waiting...");
+        try {
+          sock = servsock.accept();
+          System.out.println("Accepted connection : " + sock);
+          // send file
+          File myFile = new File (FILE_TO_SEND);
+          byte [] mybytearray  = new byte [(int)myFile.length()];
+          fis = new FileInputStream(myFile);
+          bis = new BufferedInputStream(fis);
+          bis.read(mybytearray,0,mybytearray.length);
+          os = sock.getOutputStream();
+          System.out.println("Sending " + FILE_TO_SEND + "(" + mybytearray.length + " bytes)");
+          os.write(mybytearray,0,mybytearray.length);
+          os.flush();
+          System.out.println("Done.");
+        }
+        finally {
+          if (bis != null) bis.close();
+          if (os != null) os.close();
+          if (sock!=null) sock.close();
+        }
+      }
+    }
+    finally {
+      if (servsock != null) servsock.close();
+    }
+  }    
+}
---
+++ zaslani_souboru/src/zaslani_souboru/Zaslani_souboru.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zaslani_souboru;
+
+/**
+ *
+ * @author RadimP
+ */
+public class Zaslani_souboru {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
